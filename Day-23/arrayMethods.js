72704bbea02649ffddf5dc3e093f7141d350dc3a
Neeraj Kumar Maurya <36784272+1720mca1054@users.noreply.github.com>
2018-08-31 21:06:55 +0530

    Array Methods

---
+++ Day-23/arrayMethods.js
@@ -0,0 +1,27 @@
+let family =["neeraj", "kavyansh", "agreema"];
+console.log(family);
+console.log(family.shift());
+console.log(family);
+console.log(family.unshift("neeraj maurya"));
+console.log(family);
+
+
+// above is to change things at start of array
+
+console.log(family.pop());
+console.log(family);
+
+console.log(family.push("agreema"));
+console.log(family);
+
+//above is to change things at end of array
+
+
+console.log(family.splice(1, 2, "Family One", "family two"));
+console.log(family);
+
+
+
+
+
+
