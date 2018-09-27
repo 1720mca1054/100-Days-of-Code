e438df29bbd6e3001421162fc83e9f22ddaf6eb2
Neeraj Kumar Maurya <36784272+1720mca1054@users.noreply.github.com>
2018-09-27 23:42:17 +0530

    string-demo

---
+++ Day-39/string.php
@@ -0,0 +1,10 @@
+<?php
+$firstName = "neeraj";
+$lastName = "maurya";
+print("Hello, my name is $firstName<br>");
+print("Length of my first name is ".strlen($firstName)."<br>");
+$fullName = $firstName." ".$lastName;
+print("My full name is: ".$fullName."<br>");
+print("Position of First Name is: ".strpos($fullName, $firstName)."<br>");
+print("Position of last name is :".strpos($fullName, $lastName));
+?>
