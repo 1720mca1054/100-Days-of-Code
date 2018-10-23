a15136bb35e0015a75387961d61fd874ac2aad1f
Neeraj Kumar Maurya <36784272+1720mca1054@users.noreply.github.com>
2018-10-23 23:27:07 +0530

    Update associative-array.php

--- Day-40/associative-array.php
+++ Day-40/associative-array.php
@@ -1,6 +1,6 @@
 <?php
          /* First method to associate create array. */
-         $salaries = array("mohammad" => 2000, "qadir" => 1000, "zara" => 500);
+         $salaries = array("mohammad" => 200, "qadir" => 1000, "zara" => 500);
          
          echo "Salary of mohammad is ". $salaries['mohammad'] . "<br />";
          echo "Salary of qadir is ".  $salaries['qadir']. "<br />";
